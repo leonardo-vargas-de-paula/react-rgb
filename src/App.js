f5e66914bb8c9755a9e24f4aecb7c0e659879257
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-02-21 09:56:40 -0400

    feat: adiciona innputs de range de 0 a 255 que representam rgb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useState } from 'react';
+
 function App() {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const changeToRed = ({ target }) => {
+    setRed(parseInt(target.value));
+  }
+
+  const changeToGreen = ({ target }) => {
+    setGreen(parseInt(target.value));
+  }
+
+  const changeToBlue = ({ target }) => {
+    setBlue(parseInt(target.value));
+  }
+
   return (
-    <div className="App">
-      hi
-    </div>
+    <>
+      <input type="range" min="0" max="255" value={red} onChange={changeToRed} />
+      <h3>Red: {red}</h3>
+
+      <input type="range" min="0" max="255" value={green} onChange={changeToGreen} />
+      <h3>Green: {green}</h3>
+
+      <input type="range" min="0" max="255" value={blue} onChange={changeToBlue} />
+      <h3>Blue: {blue}</h3>
+    </>
+
   );
 }
+
+
+
 
 export default App;
