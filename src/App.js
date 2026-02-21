5c297cd364618f52ba1515da90bfc2a9ebce1aa8
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-02-21 10:26:32 -0400

    refactor: substitui os inputs por um componente input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import ColorBox from './components/ColorBox';
+import RangeInput from './components/RangeInput';
 
 function App() {
   const [red, setRed] = useState(0);
@@ -21,14 +22,16 @@ function App() {
 
   return (
     <>
-      <input type="range" min="0" max="255" value={red} onChange={changeToRed} />
+      
       <h3>Red: {red}</h3>
+      <RangeInput min="0" max="255" value={red} onChange={changeToRed} />
 
-      <input type="range" min="0" max="255" value={green} onChange={changeToGreen} />
+      
       <h3>Green: {green}</h3>
-
-      <input type="range" min="0" max="255" value={blue} onChange={changeToBlue} />
+      <RangeInput min="0" max="255" value={green} onChange={changeToGreen} />
+      
       <h3>Blue: {blue}</h3>
+      <RangeInput min="0" max="255" value={blue} onChange={changeToBlue} /> 
 
       <ColorBox backgroundColor={`rgb(${red}, ${green}, ${blue})`}/>
 
