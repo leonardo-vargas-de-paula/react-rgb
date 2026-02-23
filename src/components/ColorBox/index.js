80564f5613e17ad03b69655d4776915231d3d542
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-02-22 21:30:30 -0400

    feat: define dinamicamente a classe da div do comonente colorbox

--- src/components/ColorBox/index.js
+++ src/components/ColorBox/index.js
@@ -1,12 +1,12 @@
 import "./styles.css"
 
-function ColorBox({backgroundColor}){
+function ColorBox({backgroundColor, type}){
     const [r, g, b] = backgroundColor.match(/\d+/g).map(Number);
     const isTooLight = (r + g + b) > 750;
 
 return (
     <div 
-      className={`color-box ${isTooLight ? 'is-too-light' : ''}`} 
+      className={`color-box-${type === 'history' ? 'history' : 'main'} ${isTooLight ? 'is-too-light' : ''}`} 
       style={{ backgroundColor }}
     ></div>
 );
