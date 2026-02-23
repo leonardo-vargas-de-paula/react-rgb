16a324e100e7d669beedc2304b7c3c525e744541
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-02-22 21:31:12 -0400

    feat: adiciona um pequeno preview da cor no history

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,11 +1,14 @@
 import "./styles.css";
+import ColorBox from "../ColorBox";
 
 function History({ data }) {
 
     const getColor = (rgb) => (
-        <li key={rgb.join()}>
-            {rgb.join(", ")}
-        </li>
+        <div className="history-response">
+            <li key={rgb.join()}>{rgb.join()}</li>
+            <ColorBox backgroundColor={`rgb(${rgb.join(',')})`} type="history" />  
+        </div>
+       
     )
     return (
         <section>
