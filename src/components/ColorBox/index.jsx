5eb0940132ea09b10022967de0317f3cbdff42ed
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-02-26 08:11:52 -0400

    refactor: remove gambiarra e passa width, height e border radius via props

--- src/components/ColorBox/index.jsx
+++ src/components/ColorBox/index.jsx
@@ -1,13 +1,13 @@
 import "./styles.css"
 
-function ColorBox({backgroundColor, type}){
+function ColorBox({backgroundColor, width, height, borderRadius}) {
     const [r, g, b] = backgroundColor.match(/\d+/g).map(Number);
     const isTooLight = (r + g + b) > 750;
 
 return (
     <div 
-      className={`color-box-${type === 'history' ? 'history' : 'main'} ${isTooLight ? 'is-too-light' : ''}`} 
-      style={{ backgroundColor }}
+      className={`color-box ${isTooLight ? 'is-too-light' : ''}`} 
+      style={{ backgroundColor, width, height, borderRadius }}
     ></div>
 );
 
